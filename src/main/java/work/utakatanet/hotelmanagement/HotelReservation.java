eb1f3440d9cb3458b78ba50308a60379cfa7dfb3
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-05-27 13:57:41 +0900

    Add files via upload

---
+++ src/main/java/work/utakatanet/hotelmanagement/HotelReservation.java
@@ -0,0 +1,34 @@
+package work.utakatanet.hotelmanagement;
+
+import java.util.Date;
+
+public class HotelReservation {
+
+    private Date checkInDate;
+    private Date checkOutDate;
+    private int totalPrice;
+
+    public HotelReservation(Date checkInDate, Date checkOutDate) {
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.totalPrice = calculateTotalPrice();
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    private int calculateTotalPrice() {
+        // 宿泊日数に応じた金額を計算する処理を記述する
+        return 0;
+    }
+
+}
